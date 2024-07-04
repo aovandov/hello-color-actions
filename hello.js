5486f9f8cc1c0be7f0e0eb2752ab4a705d4d9476
aovandov <alvaro.ovando@gmail.com>
2024-07-03 22:54:32 -0400

    mi primer cambio

--- hello.js
+++ hello.js
@@ -1,4 +1,4 @@
-const hello = () => "Hello World from the Office Hours in the terminal!";
+const hello = () => "Hello World prueba de Alvaro Ovando!";
 console.log(hello());
 exports.hello = hello;
 
